acfc1e148b63cdfc923dd68331d060e0c97e3355
Georg Gadinger <nilsding@nilsding.org>
2018-11-28 21:01:51 +0100

    Generate 16 different files for testing colour handling

--- test.cr
+++ test.cr
@@ -6,6 +6,22 @@ include Crixel
 width = 256_u32
 height = 256_u32
 
+(1..16).each do |mod|
+  img = Image.new(width, height)
+
+  color = 0_u32
+  height.times do |y|
+    width.times do |x|
+      color = 0xFF0000_u32 + ((y / mod) * mod)
+      img.set(x, y, color)
+    end
+  end
+
+  File.open("./img-#{mod}.sixel", "w") do |f|
+    img.render_full f
+  end
+end
+
 img = Image.new(width, height)
 
 color = 0_u32
