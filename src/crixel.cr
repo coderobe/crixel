f70326511b37e8c12959dbc7688fc109a8f6a867
Robin Broda <robin@broda.me>
2018-12-01 13:45:00 +0100

    Rewrite rendering, Add naive but correct renderer

--- src/crixel.cr
+++ src/crixel.cr
@@ -2,128 +2,152 @@
 module Crixel
   VERSION = "0.2.0"
 
-  class Squot
-    property color
+  class SixelImage
+    property width : UInt32
+    property height : UInt32
+    property pixels : Array(UInt32)
+    property target : IO
 
-    def initialize(@color : UInt32 = 0xFF000000_u32)
-    end
-
-    def set(@color : UInt32)
-    end
-
-    def transparent? : Bool
-      color & 0xFF000000 == 0xFF000000
-    end
-  end
-
-  class Sixel
-    property squots
-
-    def initialize(@squots = Array(Squot).new(6) { Squot.new })
-    end
-
-    def set(y, color : UInt32)
-      @squots[y].set(color)
-    end
-
-    def to_char : Char
-      c = 0b00000000
-      @squots.each.with_index do |sq, i|
-        c |= 1 << i unless sq.transparent?
-      end
-      (c + 63).chr
-    end
-  end
-
-  class MonochromeSixel < Sixel
-    property color
-
-    def initialize(@color : UInt32)
-      super()
-    end
-  end
-
-  class Image
-    property sixbuf : Array(Sixel)
-
-    def initialize(@width : UInt32, @height : UInt32)
-      @sixbuf = Array(Sixel).new((@width * (@height.to_f / 6).ceil).to_u32) { Sixel.new }
+    def initialize(@width, @height)
+      @pixels = Array(UInt32).new((@width)*(@height), 0)
+      @target = STDOUT
     end
 
     def set(x, y, color : UInt32)
-      sindex = (y.to_f / 6).floor.to_u32 * @width + x
-      @sixbuf[sindex].set(y % 6, color)
+      @pixels[y*@width + x] = color
     end
 
-    def render_full(io = STDOUT)
-      io.write "\033Pq".to_slice # sixel
-      render(io)
-      io.write "\033\\".to_slice # sixel exit
+    def set_target(io)
+      @target = io
     end
 
-    def split_sixel(sixel : Sixel) : Array(MonochromeSixel)
-      colors = [] of UInt32
-      sixels = [] of MonochromeSixel
-      sixel.squots.each.with_index do |sq, i|
-        color = colors.index(sq.color)
-        if color.nil?
-          color = colors.size
-          colors << sq.color
-        end
-        sixels << MonochromeSixel.new(sq.color) if sixels.size < color + 1
-        sixels[color].set(i, sq.color)
-      end
-      sixels
+    def write(msg : String)
+      @target.write(msg.to_slice)
     end
 
-    def render(io = STDOUT)
-      frame = ""
-      frame_palette = ""
-      palette = [] of UInt32
+    def render
+      colormap = Hash(UInt32, Tuple(Int32, String)).new
+      outlines = Array(Array(Hash(UInt32, String))).new
 
-      @sixbuf.each_slice(@width) do |sixline|
-        colors = [] of UInt32
-        lines = [""]
-        sixline.each do |sixel|
-          sixels = split_sixel(sixel)
-          sixels.each.with_index do |sx, i|
-            color = colors.index(sx.color)
-            if color.nil?
-              color = colors.size
-              colors << sx.color
-            end
-            if lines.size < colors.size
-              lines << 63.chr.to_s*lines.first.size
-            end
-            lines = lines.map_with_index do |line, id|
-              if id == color
-                line + sx.to_char
-              elsif line.size < lines.max_by { |a| a.size }.size
-                line + 63.chr
-              else
-                line
+      @pixels.each_slice(@width*6).with_index do |sixrow, sindex|
+        # in sixel row
+        line_collector = Array(Hash(UInt32, String)).new
+        this_line_collector = Hash(UInt32, String).new
+        @width.times do |x|
+          # in sixel col of row
+          sixel_collector = Hash(UInt32, Array(UInt8)).new
+          6.times do |ct|
+            # in sixel y of col
+            begin
+              color = sixrow[@width*ct + x]
+              sixel_collector.fetch(color) do |color|
+                sixel_collector[color] = Array(UInt8).new
+                sixel_collector.max_by { |_, a| a.size }.size.times do |t|
+                  sixel_collector[color] << 0
+                end
               end
+              sixel_collector[color] = sixel_collector.fetch(color, Array(UInt8).new) << (1 << ct).to_u8
+              sixel_collector.each do |collector_color, sixelpack|
+                if collector_color != color
+                  sixel_collector[collector_color] = sixelpack << 0
+                end
+              end
+              colstr = colormap.fetch(color, nil)
+              if colstr.nil? # color undefined
+                colstr = "##{colormap.size};2;"
+                colstr += "#{((color >> 16 & 0xFF) / 2.56).round.to_u8}"
+                colstr += ';'
+                colstr += "#{((color >> 8 & 0xFF) / 2.56).round.to_u8}"
+                colstr += ';'
+                colstr += "#{((color & 0xFF) / 2.56).round.to_u8}"
+                colormap[color] = {colormap.size, colstr}
+              end
+            rescue e : IndexError
+              break # last row of image doesn't fill entire sixel
+            end
+          end
+          sixel_collector.each do |color, sixelpack|
+            this_line_collector[color] = this_line_collector.fetch(color, "") + (63 + sixelpack.reduce { |s1, s2| s1 | s2 }).chr
+          end
+          line_collector << this_line_collector
+        end
+        outlines << line_collector
+      end
+
+      colormap.each { |ck, cs| puts cs.last }
+      outlines.each do |line_collect|
+        lastcolor = nil
+        line_collect.each do |sixel_collect|
+          sixel_collect.each do |color, linepart|
+            if lastcolor != color
+              write "##{colormap[color].first}"
+            end
+            write linepart
+            lastcolor = color
+          end
+          write "$"
+        end
+        write "-"
+      end
+    end
+
+    def render_naive
+      colors = Array(UInt32).new
+
+      @pixels.each_slice(@width*6).with_index do |sixrow, sindex|
+        # in sixel row
+        lastcolor = nil
+        lastchar = nil
+        lastchar_count = 0
+        @width.times do |x|
+          # in sixel col of row
+          6.times do |ct|
+            # in sixel y of col
+            begin
+              write "$"
+
+              color = sixrow[@width*ct + x]
+              colid = colors.index(color)
+              if colid.nil? # color undefined
+                write "##{colors.size};2;"
+                write "#{((color >> 16 & 0xFF) / 2.56).round.to_u8}"
+                write ";"
+                write "#{((color >> 8 & 0xFF) / 2.56).round.to_u8}"
+                write ";"
+                write "#{((color & 0xFF) / 2.56).round.to_u8}"
+                colors << color
+              elsif lastcolor != color
+                write "##{colid}"
+              end
+
+              if x > 1
+                write "!#{x}?"
+              else
+                write "?"
+              end
+
+              write (63 + (1 << ct)).chr.to_s
+
+              lastcolor = color
+            rescue e : IndexError
+              break # last row of image doesn't fill entire sixel
             end
           end
         end
-
-        frame_line = ""
-        lines.each.with_index do |line, index|
-          line = line.chars.chunk { |e| e }.map { |chunk| ['!', chunk.last.size, chunk.first] }.flatten.join("") # RLE
-          color = colors[index]
-          pindex = palette.index color
-          if pindex.nil? # color undefined
-            pindex = palette.size
-            palette.push color
-            frame_palette += "##{pindex};2;#{((color >> 16 & 0xFF) / 2.56).to_u8};#{((color >> 8 & 0xFF) / 2.56).to_u8};#{((color & 0xFF) / 2.56).to_u8}"
-          end
-          frame_line += "##{pindex}#{line}$"
-        end
-
-        frame += frame_line + '-'
+        write "-"
       end
+    end
 
-      io.write (frame_palette + frame).to_slice
+    def render_full
+      write "\033Pq" # sixel
+      render
+      write "\033\\" # sixel exit
+    end
+
+    def render_naive_full
+      write "\033Pq" # sixel
+      render_naive
+      write "\033\\" # sixel exit
     end
   end
 end
